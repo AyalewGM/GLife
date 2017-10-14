7f210ebce1a179d805af3fd8bcf69dd3dbd0c485
AyalewGM <ayalew.mersha@gmail.com>
2017-10-14 16:24:39 -0500

    My Last Commit

--- Manibrot/mainScript.js
+++ Manibrot/mainScript.js
@@ -34,7 +34,8 @@ var workers;
       workers = [];
       workerCount = count;
       for (i = 0; i < workerCount; i++) {
-         workers[i] = new Worker("MandelbrotWorker.js");
+         workers[i] = new Worker("https://github.com/AyalewGM/GLife/tree/master/Manibrot/MandelbrotWorker.js");
+         //workers[i] = new Worker(compue);
          workers[i].onmessage = jobFinished;
       }
    }
